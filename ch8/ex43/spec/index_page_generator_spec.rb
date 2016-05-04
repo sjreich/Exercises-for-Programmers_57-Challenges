8cd8f12f8c32cbaf285771da081a63de0d0ba16e
Joe Reich <reich.sj1@gmail.com>
2016-05-04 00:12:47 -0400

    clean up specs on website generator

--- ch8/ex43/spec/index_page_generator_spec.rb
+++ ch8/ex43/spec/index_page_generator_spec.rb
@@ -1,20 +1,28 @@
 require_relative '../index_page_generator'
 
 RSpec.describe IndexPageGenerator do
-  it 'is a class' do
-    expect( IndexPageGenerator ).to be_a Class
-  end
-
-  name = "my website"
+  let(:name) { "my website" }
   
-  it 'generates without css or js' do
-    actual_output = IndexPageGenerator.new(name).generate
-    expected_output = "<!DOCTYPE html><head></head><body><div id='title-block'><h1 id='title'>#{name}</h1></div></body>"
-    expect( actual_output ).to eq expected_output
+  context 'no js or css demanded' do
+    it 'generates the basic html' do
+      actual_output = IndexPageGenerator.new(name).generate
+      expected_output = "<!DOCTYPE html><head></head><body><div id='title-block'><h1 id='title'>#{name}</h1></div></body>"
+      expect(actual_output).to eq expected_output
+    end
   end
 
-  it 'generates with css and js' do
-    actual_output = IndexPageGenerator.new(name, true, true).generate
-    expect( actual_output ).to include "src='js/script.js'"
+  context 'with js demanded' do
+    it 'includes js' do
+      actual_output = IndexPageGenerator.new(name, false, true).generate
+      expect(actual_output).to include "<script type='text/javascript' src='js/script.js'></script>"
+    end
   end
+
+  context 'with css demanded' do
+    it 'includes css' do
+      actual_output = IndexPageGenerator.new(name, true, false).generate
+      expect(actual_output).to include "<link rel='stylesheet' type='text/css' href='css/stylesheet.css'>"
+    end
+  end
+
 end
--- ch8/ex43/spec/website_generator_spec.rb
+++
@@ -1,9 +0,0 @@
-require_relative '../website_generator'
-
-describe WebsiteGenerator do
-  it 'is a class' do
-    expect( WebsiteGenerator ).to be_a Class
-  end
-
-  
-end
